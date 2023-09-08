6e8347015bf21a206da7e3429bd0da0bbb937610
Akexorcist <akexorcist@gmail.com>
2023-09-09 03:01:36 +0700

    Fix fullscreen issue when undecorated is enabled

--- src/main/kotlin/com/akexorcist/screensharing/ui/App.kt
+++ src/main/kotlin/com/akexorcist/screensharing/ui/App.kt
@@ -2,18 +2,13 @@
 
 package com.akexorcist.screensharing.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.*
 import com.akexorcist.screensharing.di.AppModule
 import com.akexorcist.screensharing.ui.main.MainRoute
@@ -21,6 +16,8 @@ import com.akexorcist.screensharing.ui.main.MainViewModel
 import com.akexorcist.screensharing.ui.theme.defaultColors
 import com.github.eduramiba.webcamcapture.drivers.NativeDriver
 import com.github.sarxos.webcam.Webcam
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.core.context.startKoin
 import org.koin.java.KoinJavaComponent.get
 
@@ -31,6 +28,7 @@ fun main() = application {
         position = WindowPosition.Aligned(Alignment.Center),
         size = DpSize(1280.dp, 720.dp),
     )
+    val coroutineScope = rememberCoroutineScope()
     Window(
         title = "Screen Sharing",
         state = windowState,
@@ -40,8 +38,14 @@ fun main() = application {
         MaterialTheme(colors = defaultColors) {
             WindowDraggableArea {
                 App(
-                    isFullScreen = windowState.placement == WindowPlacement.Fullscreen,
-                    onMaximizeWindowClick = { windowState.placement = WindowPlacement.Fullscreen },
+                    isFullScreen = windowState.placement == WindowPlacement.Fullscreen || windowState.placement == WindowPlacement.Maximized,
+                    onMaximizeWindowClick = {
+                        coroutineScope.launch {
+                            windowState.placement = WindowPlacement.Fullscreen
+                            delay(1000)
+                            windowState.placement = WindowPlacement.Maximized
+                        }
+                    },
                     onMinimizeWindowClick = { windowState.placement = WindowPlacement.Floating },
                     onCloseAppClick = { exitApplication() },
                 )
