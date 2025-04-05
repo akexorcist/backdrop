10a1e42421f54a7107107cf1bca6401c7de7a3c8
Akexorcist <akexorcist@gmail.com>
2025-04-05 16:23:26 +0700

    fix: add border to frame rate text

--- src/main/kotlin/com/akexorcist/backdrop/ui/main/MainScreen.kt
+++ src/main/kotlin/com/akexorcist/backdrop/ui/main/MainScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.layout.ContentScale
@@ -253,6 +254,14 @@ private fun FrameRateInformation(frameRate: Double?) {
             ),
         contentAlignment = Alignment.Center,
     ) {
+        Text(
+            text = "${displayFrameRate?.toInt() ?: 0}",
+            color = MaterialTheme.colors.background,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.subtitle1.copy(
+                drawStyle = Stroke(width = 6.dp.value)
+            ),
+        )
         Text(
             text = "${displayFrameRate?.toInt() ?: 0}",
             color = MaterialTheme.colors.onBackground,
